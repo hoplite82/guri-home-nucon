c74473e47143e70fc288047d216fa2a3452345f8
robert pratersch <robert@pratersch.de>
2023-12-15 11:45:46 +0100

    rosa

--- components/Chakra.vue
+++ components/Chakra.vue
@@ -7,25 +7,31 @@
           <h2 class="mt-0">Was ist Chakra-Yoga</h2>
           <hr class="divider divider-light" />
           <p class="mb-4 fs-4">
-            Yoga ist das Beruhigen der Gedanken im Geist. Es ist eine wunderbare Möglichkeit mit uns selbst in Kontakt zu treten, bewusster zu werden und Frieden zu schaffen.
-            Chakren sind die „Energiezentren“ unseres Systems, durch die Energie aufgenommen, transformiert und abgegeben wird. 
-            Jedes Chakra ist verantwortlich für verschiedene Bereiche unseres Lebens, sowohl auf körperlicher als auch auf mentaler Ebene. Denn jedes Chakra kann bestimmten Organen, Drüsen und Gemütszuständen zugeordnet werden.  
+            Yoga ist das Beruhigen der Gedanken im Geist. Es ist eine wunderbare Möglichkeit mit uns selbst in Kontakt zu treten,
+            bewusster zu werden und Frieden zu schaffen.Chakren sind die „Energiezentren“ unseres Systems, durch die Energie
+            aufgenommen, transformiert und abgegeben wird. Jedes Chakra ist verantwortlich für verschiedene Bereiche
+            unseres Lebens, sowohl auf körperlicher als auch auf mentaler Ebene. Denn jedes Chakra kann bestimmten Organen,
+            Drüsen und Gemütszuständen zugeordnet werden.  
+          </p>  
+          <p class="mb-4 fs-4">  
             Sind die Chakren offen und gesund, dann funktionieren sie einwandfrei und versorgen unser gesamtes System mit Energie. Probleme treten auf, wenn die Chakren blockiert sind und daraus resultierend der gesamte Energiefluss gestört wird. 
-            Die Chakren zeigen uns, wie alles zusammen hängt, Wir sind weder nur Körper, noch nur Seele oder nur Geist. Wir sind eine wunderbare Einheit.
           </p>
-        </div>
-        <br /><br />
-        <div class="col-lg-8 text-center">
+          <p class="mb-4 fs-4">
+            Die Chakren zeigen uns, wie alles zusammen hängt, wir sind weder nur Körper, noch nur Seele oder nur Geist. Wir sind eine wunderbare Einheit.
+          </p>
+        
+        
+        
           <h2 class="mt-0">Gesundheit über den Weg des Chakra Yoga</h2>
-          <hr class="divider divider-light" />
+          
           <p class="mb-4 fs-4">
             Dysbalancen auf körperlicher oder geistiger Ebene können wir mithilfe der Chakren besser verstehen. Mit dem Chakrensystem habe ich eine Art Fahrplan und die Möglichkeit der Selbstheilung. 
             Innere Ruhe finden wir durch das bewusste Anhalten, Atmen und Beobachten was in dir geschieht, welche Gedanken, Gefühle und Reaktionen zeigen sich. Eigene Themen mit Wahrhaftigkeit anschauen, annehmen und loslassen. Durch die Yogapraxis schaffen wir den Raum uns dessen anzunehmen.
           </p>
-        </div>
-        <br />
+        
+        
         <div>
-          <img src="@/assets/img/chakra_explain_fig.jpg" />
+          <img src="@/assets/img/chakras7.png" />
         </div>
         <div>
         <h2>Hauptchakren</h2>  
@@ -49,6 +55,7 @@
         </ol>
         </div>
       </div>
+    </div>
     </div>
   </section>
 </template>
