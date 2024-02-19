c9dc29a6e06f79c139ba83aca2acfcd34a3d0fd3
robert pratersch <robert@pratersch.de>
2024-02-19 23:53:13 +0100

    before scss

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,33 +1,35 @@
 <template>
-    <header class="masthead">
+  <header class="masthead">
     <div class="container px-4 px-lg-5 h-75">
-        <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
-          <div class="col-lg-8 align-self-end">
-            <h1 class="text-white font-weight-bold">Chakra-Yoga</h1> 
-            <h3 class="text-white-75 mb-5 kaushan">Finde deine natürliche Balance</h3>
-            <!--<div><img class="" src="@/assets/img/Buddha.png" /></div>-->
-          </div>
-          <div class="col-lg-8 align-self-baseline">
-            <!--  <a class="btn btn-primary btn-xl" href="#aboutme">Find Out More</a> -->
-          </div>
+      <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-8 align-self-end">
+          <h1 class="text-white font-weight-bold">Chakra-Yoga</h1>
+          <h3 class="text-white-75 mb-5 kaushan">Finde deine natürliche Balance</h3>
+          <!--<div><img class="" src="@/assets/img/Buddha.png" /></div>-->
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <!--  <a class="btn btn-primary btn-xl" href="#aboutme">Find Out More</a> -->
         </div>
       </div>
-    </header>
+    </div>
+  </header>
 </template>
 <style>
 header.masthead {
   padding-top: 10rem;
   padding-bottom: calc(10rem - 4.5rem);
-  background: linear-gradient(to bottom, rgba(92, 77, 66, 0.4) 0%, rgba(92, 77, 66, 0.4) 100%), url("@/assets/img/bg-masthead.jpg");
+  background: linear-gradient(to bottom, rgba(92, 77, 66, 0.4) 0%, rgba(92, 77, 66, 0.4) 100%),
+    url("@/assets/img/bg-masthead.jpg");
   background-position: center;
   background-repeat: no-repeat;
   background-attachment: scroll;
   background-size: cover;
 }
-header.masthead h1, header.masthead .h1 {
+header.masthead h1,
+header.masthead .h1 {
   font-size: 2.25rem;
 }
-@media (min-width: 992px) {
+@media (min-width: 800px) {
   header.masthead {
     height: 100vh;
     min-height: 40rem;
@@ -37,12 +39,14 @@ header.masthead h1, header.masthead .h1 {
   header.masthead p {
     font-size: 1.15rem;
   }
-  header.masthead h1, header.masthead .h1 {
+  header.masthead h1,
+  header.masthead .h1 {
     font-size: 3rem;
   }
 }
 @media (min-width: 1200px) {
-  header.masthead h1, header.masthead .h1 {
+  header.masthead h1,
+  header.masthead .h1 {
     font-size: 3.5rem;
   }
 }
