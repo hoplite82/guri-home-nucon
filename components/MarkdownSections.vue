22b1882881e0e6015fbc7999f05dfc0665fd90a6
robert pratersch <robert@pratersch.de>
2024-02-24 15:27:28 +0100

    img grap

--- components/MarkdownSections.vue
+++ components/MarkdownSections.vue
@@ -3,34 +3,30 @@ function rosaswitch(num) {
   return num % 2 === 0 ? "bg-rosa" : "";
 }
 // const textblocks = await queryContent("textblock").find();
-</script>
-<style scoped>
-  img {
-    max-width: 100%;
-    height: auto;
-  }
-</style>
+</script> 
+
 <template>
   <ContentList path="/textblock" v-slot="{ list }">
-    
-      <section
-        v-for="tb in list
-          .sort((a, b) => a.id - b.id)
-          .slice(1)
-          .filter((tb) => tb.show)"
-        :key="tb.id"
-        :id="tb.section"
-        :class="['page-section', rosaswitch(tb.id)]">
-        <div class="container">
+    <section
+      v-for="tb in list
+        .sort((a, b) => a.id - b.id)
+        .slice(1)
+        .filter((tb) => tb.show)"
+      :key="tb.id"
+      :id="tb.section"
+      :class="['page-section', rosaswitch(tb.id)]">
+      <div class="container">
+        <div class="row">
           <h2 class="h2">{{ tb.title }}</h2>
           <hr class="divider divider-light" />
 
-          <div v-if="tb.bild">
-            <NuxtImg src="/img/nina_boot.jpg" sizes="50vw sm:50vw md:300px lg:500px" style="float: left" class="m-2" />
-          </div>
-          <ContentRendererMarkdown :value="tb.body" />
+          
+          <NuxtImg v-if="tb.bild" src="/img/nina_boot.jpg" sizes="50vw sm:50vw md:300px lg:500px" style="float: left" class="m-2" />
+         
+          <ContentRendererMarkdown :value="tb.body" class="markdown"/>
+         
         </div>
-      </section>
-    
+      </div>
+    </section>
   </ContentList>
 </template>
